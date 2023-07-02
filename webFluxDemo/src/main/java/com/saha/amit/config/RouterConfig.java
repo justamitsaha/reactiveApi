fc0ac09f62e0a133a707f26962b4a79cbeac585f
justamitsaha <justamitsaha@gmail.com>
2023-07-02 12:34:17 +0530

    Input validation Exception

--- webFluxDemo/src/main/java/com/saha/amit/config/RouterConfig.java
+++ webFluxDemo/src/main/java/com/saha/amit/config/RouterConfig.java
@@ -1,13 +1,20 @@
 package com.saha.amit.config;
 
+import java.util.function.BiFunction;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import com.saha.amit.dto.ExceptionResponse;
+import com.saha.amit.exception.InputValidationException;
 import com.saha.amit.service.RequestHandeler;
+
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class RouterConfig {
@@ -20,6 +27,21 @@ public class RouterConfig {
 		return RouterFunctions.route()
 				.GET("router/square/{input}", requestHandeler::squareHandler)
 				.GET("router/table/{input}", requestHandeler::tableHandler)
+				.POST("router/multiply",requestHandeler::multiplyHandler)
+				.GET("router/square/{input}/validation", requestHandeler::squareHandlerWithValidation)
+                .onError(InputValidationException.class, exceptionHandler())
 				.build();
 	}
+	
+	
+	private BiFunction<Throwable, ServerRequest, Mono<ServerResponse>> exceptionHandler(){
+        return (err, req) -> {
+            InputValidationException ex = (InputValidationException) err;
+            ExceptionResponse response = new ExceptionResponse();
+            response.setInput(ex.getInput());
+            response.setMessage(ex.getMessage());
+            response.setErrorCode(ex.getErrorCode());
+            return ServerResponse.badRequest().bodyValue(response);
+        };
+    }
 }
--- webFluxDemo/src/main/java/com/saha/amit/service/RequestHandeler.java
+++ webFluxDemo/src/main/java/com/saha/amit/service/RequestHandeler.java
@@ -1,11 +1,14 @@
 package com.saha.amit.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import com.saha.amit.dto.MultiplyDto;
 import com.saha.amit.dto.Response;
+import com.saha.amit.exception.InputValidationException;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -25,6 +28,23 @@ public class RequestHandeler {
 	public Mono<ServerResponse> tableHandler(ServerRequest serverRequest){
 		int input = Integer.parseInt(serverRequest.pathVariable("input"));
 		Flux<Response> mono = reactiveMathService.getMultiplicationTable(input);
+		return ServerResponse.ok()
+				.contentType(MediaType.TEXT_EVENT_STREAM)
+				.body(mono, Response.class);
+	}
+	
+	public Mono<ServerResponse>multiplyHandler(ServerRequest serverRequest){
+		Mono<MultiplyDto> requestDtoMono = serverRequest.bodyToMono(MultiplyDto.class);
+		Mono<Response> mono = reactiveMathService.multiply(requestDtoMono);
+		return ServerResponse.ok().body(mono, Response.class);
+	}
+	
+	public Mono<ServerResponse> squareHandlerWithValidation(ServerRequest serverRequest){
+		int input = Integer.parseInt(serverRequest.pathVariable("input"));
+		if(input<5) {
+			return Mono.error(new InputValidationException(input));
+		}
+		Mono<Response> mono = reactiveMathService.findSquare(input);
 		return ServerResponse.ok().body(mono, Response.class);
 	}
 
