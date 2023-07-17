fde29b0bcd819a8af766a859d98f1a7dcb76d877
justamitsaha <justamitsaha@gmail.com>
2023-07-17 06:36:38 +0530

    Sending query params and Auth token

--- webFluxDemo/src/test/java/com/saha/amit/config/WebClientConfig.java
+++ webFluxDemo/src/test/java/com/saha/amit/config/WebClientConfig.java
@@ -2,7 +2,11 @@ package com.saha.amit.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.ExchangeFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class WebClientConfig {
@@ -11,6 +15,13 @@ public class WebClientConfig {
     public WebClient webClient() {
         return WebClient.builder()
                 .baseUrl("http://localhost:8080/")
+                .filter((clientRequest, exchangeFunction) -> sessionToken(clientRequest,exchangeFunction))
                 .build();
+    }
+
+    private Mono<ClientResponse> sessionToken(ClientRequest request, ExchangeFunction ex) {
+        System.out.println("generating session token");
+        ClientRequest clientRequest = ClientRequest.from(request).headers(h -> h.setBearerAuth("some-lengthy-jwt")).build();
+        return ex.exchange(clientRequest);
     }
 }
