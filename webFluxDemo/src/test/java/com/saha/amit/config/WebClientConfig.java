fdc56be3334c96c45f7160c65b930c7576aebf5a
justamitsaha <justamitsaha@gmail.com>
2023-07-18 08:21:50 +0530

    Testing Attributes

--- webFluxDemo/src/test/java/com/saha/amit/config/WebClientConfig.java
+++ webFluxDemo/src/test/java/com/saha/amit/config/WebClientConfig.java
@@ -15,13 +15,33 @@ public class WebClientConfig {
     public WebClient webClient() {
         return WebClient.builder()
                 .baseUrl("http://localhost:8080/")
-                .filter((clientRequest, exchangeFunction) -> sessionToken(clientRequest,exchangeFunction))
+                .filter((clientRequest, exchangeFunction) -> sessionToken(clientRequest, exchangeFunction))
                 .build();
     }
 
-    private Mono<ClientResponse> sessionToken(ClientRequest request, ExchangeFunction ex) {
+/*    private Mono<ClientResponse> sessionToken(ClientRequest request, ExchangeFunction ex){
         System.out.println("generating session token");
         ClientRequest clientRequest = ClientRequest.from(request).headers(h -> h.setBearerAuth("some-lengthy-jwt")).build();
         return ex.exchange(clientRequest);
+    }*/
+
+    private Mono<ClientResponse> sessionToken(ClientRequest request, ExchangeFunction ex) {
+        //auth --> basic or oauth
+        ClientRequest clientRequest = request.attribute("auth")
+                .map(v -> v.equals("basic") ? withBasicAuth(request) : withOAuth(request))
+                .orElse(request);
+        return ex.exchange(clientRequest);
+    }
+
+    private ClientRequest withBasicAuth(ClientRequest request) {
+        return ClientRequest.from(request)
+                .headers(h -> h.setBasicAuth("username", "password"))
+                .build();
+    }
+
+    private ClientRequest withOAuth(ClientRequest request) {
+        return ClientRequest.from(request)
+                .headers(h -> h.setBearerAuth("some-token"))
+                .build();
     }
 }
