15c6b3e748311d2ea67d9cbb0dcfb531d74c1b2b
justamitsaha <justamitsaha@gmail.com>
2023-07-19 06:46:48 +0530

    Product Service Set up

---
+++ product-service/src/main/java/com/saha/amit/ProductServiceApplication.java
@@ -0,0 +1,7 @@
+package com.saha.amit;
+
+public class ProductServiceApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ product-service/src/main/java/com/saha/amit/dto/ProductDTO.java
@@ -0,0 +1,32 @@
+package com.saha.amit.dto;
+
+public class ProductDTO {
+
+    private String id;
+    private String Description;
+    private Integer price;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
