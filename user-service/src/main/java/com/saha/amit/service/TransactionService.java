5893d1c7743a33bb8a3893763a877277b72aaa3f
justamitsaha <justamitsaha@gmail.com>
2023-07-29 07:01:03 +0530

    Transaction Service

---
+++ user-service/src/main/java/com/saha/amit/service/TransactionService.java
@@ -0,0 +1,37 @@
+package com.saha.amit.service;
+
+import com.saha.amit.dto.TransactionRequestDto;
+import com.saha.amit.dto.TransactionResponseDto;
+import com.saha.amit.dto.TransactionStatus;
+import com.saha.amit.entity.UserTransaction;
+import com.saha.amit.repository.UserRepository;
+import com.saha.amit.repository.UserTransactionRepository;
+import com.saha.amit.util.EntityDtoUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class TransactionService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserTransactionRepository transactionRepository;
+
+    public Mono<TransactionResponseDto> createTransaction(final TransactionRequestDto requestDto){
+        return this.userRepository.updateUserBalance(requestDto.getUserId(), requestDto.getAmount())
+                .filter(Boolean::booleanValue)
+                .map(b -> EntityDtoUtil.toEntity(requestDto))
+                .flatMap(this.transactionRepository::save)
+                .map(ut -> EntityDtoUtil.toDto(requestDto, TransactionStatus.APPROVED))
+                .defaultIfEmpty(EntityDtoUtil.toDto(requestDto, TransactionStatus.DECLINED));
+    }
+
+    public Flux<UserTransaction> getByUserId(int userId){
+        return this.transactionRepository.findByUserId(userId);
+    }
+
+}
