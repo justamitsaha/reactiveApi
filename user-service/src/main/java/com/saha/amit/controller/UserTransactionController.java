0d8fe2d38971db7fcdb75e4aa11e8b949ad03918
justamitsaha <justamitsaha@gmail.com>
2023-07-30 07:30:35 +0530

    User Service complete

---
+++ user-service/src/main/java/com/saha/amit/controller/UserTransactionController.java
@@ -0,0 +1,29 @@
+package com.saha.amit.controller;
+
+import com.saha.amit.dto.TransactionRequestDto;
+import com.saha.amit.dto.TransactionResponseDto;
+import com.saha.amit.entity.UserTransaction;
+import com.saha.amit.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("user/transaction")
+public class UserTransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public Mono<TransactionResponseDto> createTransaction(@RequestBody Mono<TransactionRequestDto> requestDtoMono){
+        return requestDtoMono.flatMap(this.transactionService::createTransaction);
+    }
+
+    @GetMapping
+    public Flux<UserTransaction> getByUserId(@RequestParam("userId") int userId){
+        return this.transactionService.getByUserId(userId);
+    }
+
+}
