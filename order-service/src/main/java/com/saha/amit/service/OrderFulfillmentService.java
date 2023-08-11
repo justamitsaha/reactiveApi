aba1f7b8c6b1c82c3f83d0578ec0419ba2869247
justamitsaha <justamitsaha@gmail.com>
2023-08-11 18:27:40 +0530

    OrderFulfillmentService

---
+++ order-service/src/main/java/com/saha/amit/service/OrderFulfillmentService.java
@@ -0,0 +1,55 @@
+package com.saha.amit.service;
+
+
+import com.saha.amit.client.ProductClient;
+import com.saha.amit.client.UserClient;
+import com.saha.amit.dto.PurchaseOrderRequestDto;
+import com.saha.amit.dto.PurchaseOrderResponseDto;
+import com.saha.amit.dto.RequestContext;
+import com.saha.amit.repository.PurchaseOrderRepository;
+import com.saha.amit.util.EntityDtoUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
+
+@Service
+public class OrderFulfillmentService {
+
+    @Autowired
+    private PurchaseOrderRepository orderRepository;
+
+    @Autowired
+    private ProductClient productClient;
+
+    @Autowired
+    private UserClient userClient;
+
+    public Mono<PurchaseOrderResponseDto> processOrder(Mono<PurchaseOrderRequestDto> requestDtoMono){
+        return requestDtoMono.map(RequestContext::new)
+                .flatMap(this::productRequestResponse)
+                .doOnNext(EntityDtoUtil::setTransactionRequestDto)
+                .flatMap(this::userRequestResponse)
+                .map(EntityDtoUtil::getPurchaseOrder)
+                .map(this.orderRepository::save) // blocking
+                .map(EntityDtoUtil::getPurchaseOrderResponseDto)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    private Mono<RequestContext> productRequestResponse(RequestContext rc){
+        return this.productClient.getProductById(rc.getPurchaseOrderRequestDto().getProductId())
+                .doOnNext(rc::setProductDto)
+                .retryWhen(Retry.fixedDelay(5, Duration.ofSeconds(1)))
+                .thenReturn(rc);
+    }
+
+    private Mono<RequestContext> userRequestResponse(RequestContext rc){
+        return this.userClient.authorizeTransaction(rc.getTransactionRequestDto())
+                .doOnNext(rc::setTransactionResponseDto)
+                .thenReturn(rc);
+    }
+
+}
