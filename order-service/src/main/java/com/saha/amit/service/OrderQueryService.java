a24002e5bcf9091c8fe32bd7188fa4001337414c
justamitsaha <justamitsaha@gmail.com>
2023-08-12 13:36:24 +0530

    OrderQueryService

---
+++ order-service/src/main/java/com/saha/amit/service/OrderQueryService.java
@@ -0,0 +1,23 @@
+package com.saha.amit.service;
+
+import com.saha.amit.dto.PurchaseOrderResponseDto;
+import com.saha.amit.repository.PurchaseOrderRepository;
+import com.saha.amit.util.EntityDtoUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+@Service
+public class OrderQueryService {
+
+    @Autowired
+    private PurchaseOrderRepository orderRepository;
+
+    public Flux<PurchaseOrderResponseDto> getProductsByUserId(int userId){
+        return Flux.fromStream(() -> this.orderRepository.findByUserId(userId).stream()) // blocking
+                .map(EntityDtoUtil::getPurchaseOrderResponseDto)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+}
