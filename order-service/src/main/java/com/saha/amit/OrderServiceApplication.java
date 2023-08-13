86e3718b4a3e5f04e6e1416a6efb1c8cdfd7e60c
justamitsaha <justamitsaha@gmail.com>
2023-08-13 23:49:51 +0530

    OrderServiceApplication

---
+++ order-service/src/main/java/com/saha/amit/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.saha.amit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
